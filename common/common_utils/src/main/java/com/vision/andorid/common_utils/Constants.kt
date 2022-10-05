e095396e3d5693f4c68eddc66c612260682752a8
Himanshu <himanshugaur684@gmail.com>
2022-10-05 16:08:43 +0530

    News Presentation layer setup is completed.

--- common/common_utils/src/main/java/com/vision/andorid/common_utils/Constants.kt
+++ common/common_utils/src/main/java/com/vision/andorid/common_utils/Constants.kt
@@ -3,7 +3,7 @@ package com.vision.andorid.common_utils
 object Constants {
 
     const val BASE_URL="https://newsapi.org/v2/"
-    const val API_KEY="36091782782f4cd4831d584627cabe6e"
+    const val API_KEY=""
     const val CATEGORY="business"
     const val COUNTRY="us"
 }
